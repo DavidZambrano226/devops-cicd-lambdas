edb822526d037b265a5d1e0a0fa90e1a1dbed4f2
David Zambrano <dzambrano.226@gmail.com>
2024-06-06 20:27:59 -0500

    :construction: Adding test and linters

---
+++ hello-lambdas/test/index-hello-world.test.js
@@ -0,0 +1,10 @@
+describe("Index Hello Test", () => {
+  const indexToTest = require("../src/index-hello-world");
+  it("Hello World", async () => {
+    const response = await indexToTest.handler();
+    expect(response).toEqual({
+      statusCode: 200,
+      body: '{"message":"Hello World!"}',
+    });
+  });
+});
