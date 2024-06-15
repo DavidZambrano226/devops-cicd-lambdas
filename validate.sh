020977c30013740a18e36a9a4529acc4e99920c2
David Zambrano <dzambrano.226@gmail.com>
2024-06-14 20:46:55 -0500

    :sparkles: Delete sort key in table

--- validate.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# verify status of cloudformation stack
-status="$(aws cloudformation describe-stacks --stack-name cicd-lambda-stack --region us-east-1 --query "Stacks[0].StackStatus")"
-echo $status
-if [[ $status == *"ROLLBACK_COMPLETE"* ]]; then
-    echo "Stack is in ROLLBACK_COMPLETE state. Deleting the stack.";
-fi
